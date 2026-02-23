f5defb7179176f3be4f7d7f3ae6bff25f2fb1306
RamonE9 <202180188@psu.palawan.edu.ph>
2026-02-23 13:32:43 +0800

    org delete

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.models import Organization
 from studentorg.forms import OrganizationForm
 from django.urls import reverse_lazy
 paginate_by = 5
 
-
+class OrganizationDeleteView(DeleteView):
+    model = Organization
+    template_name = "org_confirm_delete.html"
+    success_url = reverse_lazy('organization-list')
 
 class OrganizationUpdateView(UpdateView):
     model = Organization
